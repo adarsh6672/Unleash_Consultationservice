8b1bde5d72dbe905ccff777de0933b08153fda4e
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-05 09:57:59 +0530

    new workflow

--- src/test/java/com/unleash/consultationservice/ConsultationServiceApplicationTests.java
+++ src/test/java/com/unleash/consultationservice/ConsultationServiceApplicationTests.java
@@ -2,7 +2,6 @@ package com.unleash.consultationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
