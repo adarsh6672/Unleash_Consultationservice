7a34c66ac005f4855c890cabe057241a61dde9ec
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-05 15:12:25 +0530

    updated kafka configuration

--- src/main/java/com/unleash/consultationservice/config/KafkaProducerConfig.java
+++ src/main/java/com/unleash/consultationservice/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.unleash.consultationservice.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,6 +17,9 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public NewTopic createTopic(){
         return new NewTopic("unleash-notification", 3, (short) 1);
@@ -25,7 +29,7 @@ public class KafkaProducerConfig {
     public Map<String,Object> producerConfig(){
         Map<String,Object> props=new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
