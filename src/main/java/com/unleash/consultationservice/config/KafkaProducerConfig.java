79e5aa072bcbf9cfe11b03034f1a40170b21f138
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-11 10:17:20 +0530

    kafka checked

--- src/main/java/com/unleash/consultationservice/config/KafkaProducerConfig.java
+++ src/main/java/com/unleash/consultationservice/config/KafkaProducerConfig.java
@@ -17,8 +17,6 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
 
     @Bean
     public NewTopic createTopic(){
@@ -29,7 +27,7 @@ public class KafkaProducerConfig {
     public Map<String,Object> producerConfig(){
         Map<String,Object> props=new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "kafka:9092");
+                "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
